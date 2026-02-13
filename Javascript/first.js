bc0d07a12185576a8b4af8f5628c06273d87619a
ANMOL SINGH <anmolsin1124@.com>
2026-02-13 13:47:45 +0530

    Hello my name isna mnsmol isngh hti snamt doras th

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,1 +1,10 @@
-console.log("hello my name is anmol singh thi.s id my website");
+// //create vaiables 
+// let name = "anmol singh"
+// let a = 10;
+// console.log(name,a);
+// const accout = 100;
+// accout = 45;
+// console.log(accout);var
+// var a =10;
+// a=  145;
+// console.log(a) 
