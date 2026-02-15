708f3e9bedb7e68709fb994c1c1cf404410b6485
ANMOL SINGH <anmolsin1124@.com>
2026-02-15 18:57:11 +0530

    dfsdfg

--- Javascript/first.js
+++ Javascript/first.js
@@ -8,3 +8,24 @@
 // var a =10;
 // a=  145;
 // console.log(a) 
+// console.log("int my anem si anmols gnhg");
+
+// let a   =10;
+//  a = 45;
+// console.log(a)
+// let array = [10,"anmol_singh",true,Boolean,465464654654654]
+// let a = "45";
+
+// let b = Number(a)
+// console.log( typeof b)
+// let x = 20;;
+// let y= "20";
+// console.log(x==y);
+// console.log(x===y);
+// let a = "120lk";
+// let b = Number(a);
+// console.log(b)
+let = a = 10;
+let b = String(a);
+console.log( typeof b);     
+    
