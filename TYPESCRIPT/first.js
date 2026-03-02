9505155329933aa1fdce50972f0cfd1dcbd1f92d
ANMOL SINGH <anmolsin1124@gmail.com>
2026-03-02 17:31:54 +0530

    sasda

---
+++ TYPESCRIPT/first.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let e = true;
+let f = 10;
+let bignumber = 100000000000n;
+//# sourceMappingURL=first.js.map
---
+++ TYPESCRIPT/first.ts
@@ -0,0 +1,3 @@
+let e: boolean = true;
+let f: number = 10;
+let bignumber: bigint = 100000000000n;
