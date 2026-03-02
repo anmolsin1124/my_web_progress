daed1710937a0aea1fdfc5e619efbbba99ce87dd
ANMOL SINGH <anmolsin1124@.com>
2026-03-02 11:26:19 +0530

    Update files

--- GITHUBFILES/first.js
+++ GITHUBFILES/first.js
@@ -1,7 +1,3 @@
 ///My First GitHub File
 console.log("Hello, GitHub!");
-console.log("This is my first file on GitHub.");
-function helloWorld() {
-  console.log("Hello, World!");
-}
-helloWorld();
+////cretea
