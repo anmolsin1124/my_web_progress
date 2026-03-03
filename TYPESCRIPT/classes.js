8fbfec37350d04d27457a9bf637e2da4dcb14b8f
ANMOL SINGH <anmolsin1124@gmail.com>
2026-03-03 10:38:29 +0530

    sdsd

---
+++ TYPESCRIPT/classes.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/////////////////////////////////classes in typscrip
+class Person {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const obj1 = new Person('Anmol', 50);
+const obj2 = new Person('Happy', 40);
+console.log(obj1);
+console.log(obj2);
--- TYPESCRIPT/classes.ts
+++ TYPESCRIPT/classes.ts
@@ -1,1 +1,16 @@
-git add .
+/////////////////////////////////classes in typscrip
+class Person {
+    name:string;
+    age:number;
+
+    constructor(name:string,age:number){
+        this.name = name;
+        this.age = age;
+    }
+}
+const obj1  = new Person('Anmol',50);
+const obj2  = new Person('Happy',40);
+
+console.log(obj1);
+console.log(obj2);
+
