9bda404fba073b7ccd46234efec620cbb92c0631
ANMOL SINGH <anmolsin1124@gmail.com>
2026-03-03 20:00:29 +0530

    react

---
+++ REACT/day1/app.js
@@ -0,0 +1,6 @@
+React.createElement('h1', {}, ",Hello Anmol")
+
+
+// ReactDOM.render(Element, document.getElementById('root'));
+const Reactroot = ReactDOM.createRoot(document.getElementById("root"));
+Reactroot.render(Element);
