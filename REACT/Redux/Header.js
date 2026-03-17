9e70a9d04a7f42e2c18ccd6cbdcc627de5970bc3
ANMOL SINGH <anmolsin1124@gmail.com>
2026-03-17 20:48:09 +0530

    redux Hndlink

--- REACT/Redux/Header.js
+++ REACT/Redux/Header.js
@@ -1,9 +1,13 @@
-export default function header() {
+import { useSelector } from "react-redux"
 
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
-            <h1>Swiggy</h1>
-            <h2>card</h2>
-        </div>
-    )
-}
+export default function Header() {
+
+    useSelector((state) => {
+        const count = state.slice2.count;
+        return (
+            <div style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
+                <h1>Swiggy</h1>
+                <h2>card</h2>
+            </div>
+        )
+    }
