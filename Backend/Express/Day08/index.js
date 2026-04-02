6013e27cf8f7bb514b31976856192b015bebf3c1
ANMOL SINGH <anmolsin1124@gmail.com>
2026-04-02 14:48:37 +0530

    backend

--- Backend/Express/Day08/index.js
+++ Backend/Express/Day08/index.js
@@ -10,7 +10,8 @@ const BookStore = [
 ]
 app.use(express.json())
 app.get("/book", (req, res) => {
-    res.send(BookStore);
+    const Book = BookStore.filter(info => info.author === req.query.author)
+    res.send(Book);
 })
 app.post('/book', (req, res) => {
     BookStore.push(req.body);
@@ -24,4 +25,19 @@ app.get('/book/:id', (req, res) => {
     const id = parseInt(req.params.id)
     const book = BookStore.find(info => info.id === id);
     res.send(book);
+})
+app.patch('/book', (req, res) => {
+    const book = BookStore.find(info => info.id === req.body.id);
+    if (req.body.author)
+        book.author = req.body.author;
+    if (req.body.name)
+        book.name = req.body.name
+
+    res.send("patched");
+})
+app.delete("/book/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = BookStore.findIndex(info => info.id === id);
+    BookStore.splice(index, 1);
+    res.send("Data Save Succesfully")
 })
