d9c996ac145dc5a77efa3c7c4b35a7df6de0e461
anmolsin1124 <anmolsin1124@gmail.com>
2026-06-02 08:48:20 +0530

    saddsf

---
+++ Backend/AI Agent/index.js
@@ -0,0 +1,11 @@
+const { GoogleGenAI } = reqire("@google/genai");
+const ai = new GoogleGenAI({ apikey: "" })
+async function main() {
+    const response = await ai.models.genreateContent
+}
+
+
+
+async function getweather(location) {
+    for (const { city })
+}
--- Backend/MongoDB/MCP/index.js
+++ Backend/MongoDB/MCP/index.js
@@ -1,6 +1,2 @@
-console.log("Hello Name Is Anmol Singh This Is My First MongoDB Project");
-//find error in this loop
-let i = 1;
-for (i = 1; i < 11; i++) {
-    console.log((i * 8) / 2);
-}
+const container = getelementbyid("container");
+container.style.display = "fle
